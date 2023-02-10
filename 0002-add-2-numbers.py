8381145d42d4c5f9cee9f5cc8501faebb8618dcb
gamescomputersplay <kvasov.dmi@gmail.com>
2023-02-11 01:40:44 +0800

    accepted solution

--- 0002-add-2-numbers.py
+++ 0002-add-2-numbers.py
@@ -66,6 +66,50 @@ class Solution(object):
         :rtype: ListNode
         """
 
+        def pop_link(link):
+            ''' Return value and next link.
+            0 and the same link, if it is the last one
+            '''
+            if link:
+                return link.val, link.next
+            return 0, link
+
+        # Initialize:
+        # First: link we'll return in the end
+        # Previous: link ready to be extended
+        # Carry: 1 if previous sum > 9
+        first, previous, carry = None, None, 0
+
+        # Go through 2 pointers until both have ended
+        while l1 or l2:
+
+            # Pop the values  from each
+            value_1, l1 = pop_link(l1)
+            value_2, l2 = pop_link(l2)
+
+            # Calculate value and carry for the current digit
+            current_sum = value_1 + value_2 + carry
+            current_value, carry = current_sum % 10, current_sum // 10
+
+            # Create a new link with that value
+            current = ListNode(current_value)
+
+            # Remember the first link - we'll need to return it
+            if first is None:
+                first = current
+            # Otherwise, connect  to the previous link
+            else:
+                previous.next = current
+
+            # Current link now ready to accept links
+            previous = current
+
+        #  If after going through all digit there is still carry
+        # Add last digit "1"
+        if carry == 1:
+            previous.next = ListNode(1)
+
+        return first
 
 def main():
     ''' Test the twoSum
@@ -79,6 +123,7 @@ def main():
 
     solution = Solution()
     for list_1, list_2 in test_cases:
+        print(list_1, list_2)
         print(solution.addTwoNumbers(list_1, list_2))
 
 
