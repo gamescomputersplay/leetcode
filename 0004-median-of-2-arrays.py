86c25ea4c1a08bdf5056dee6d401cace726bb23e
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-14 19:04:56 +0800

    0004 edge case

--- 0004-median-of-2-arrays.py
+++ 0004-median-of-2-arrays.py
@@ -11,6 +11,19 @@ class Solution(object):
         :rtype: float
         """
 
+        def regular_median(array):
+            ''' Return a median of a sorted array
+            '''
+            if len(array) % 2 == 0:
+                return (array[len(array) // 2] + array[(len(array) // 2) - 1]) / 2
+            return float(array[len(array) // 2])
+
+        # Sanitizing: one of the inputs is empty
+        # (assuming that at least one of them isn't)
+        if not nums1:
+            return regular_median(nums2)
+        if not nums2:
+            return regular_median(nums1)
 
 def main():
     ''' Test findMedianSortedArrays
@@ -18,9 +31,12 @@ def main():
     solution = Solution()
 
     test_cases = [
-       ([1, 3], [2]), # 2
-       ([1, 2], [3, 4]), # 2.5
-       ([1, 2, 3, 4], [5, 6, 7, 8]), # 5.5
+        ([1, 3, 5], []), # 3
+        ([], [1, 2, 3, 4]), # 2.5
+        # ([1, 3, 5, 7], [2, 4, 6, 8]), # 4.5
+        # ([1, 3], [2]), # 2
+        # ([1, 2], [3, 4]), # 2.5
+        # ([1, 2, 3, 4], [5, 6, 7, 8]), # 5.5
     ]
     for list1, list2 in test_cases:
         print(solution.findMedianSortedArrays(list1, list2))
