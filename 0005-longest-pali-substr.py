387029c322d3d58dad988b8b6d68d6c34fd6ce9e
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-16 15:31:33 +0800

    0005 working version

--- 0005-longest-pali-substr.py
+++ 0005-longest-pali-substr.py
@@ -8,6 +8,26 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # Go through all possible lengths of pali from the biggest to 2
+        for length in range(len(s), 1, -1):
+
+            # Go through all positions where pali of that length can occur
+            for start in range(0, len(s) - length + 1):
+
+                # From out in, go through pair of letters
+                for position in range(0, length // 2):
+                    
+                    # If letters are not the same - this is not
+                    # a pali, next position
+                    if s[start + position] != s[start + length - position - 1]:
+                        break
+
+                # If there was no mismatch - it is a pali, return it!
+                else:
+                    return s[start:start+length]
+
+        # If no pali is found, return the first letter
+        return s[0]
 
 
 def main():
@@ -16,11 +36,24 @@ def main():
     solution = Solution()
 
     test_cases = [
+        "a",
+        "bb",
         "babad", # "bab" or "aba"
         "cbbd", # bb
+        "a" * 1000,
+        "ab" * 500,
+        "abcd" * 250 + "d"
     ]
+    
     for string  in test_cases:
-        print(solution.longestPalindrome(string))
+        start = time.time()
+        result = solution.longestPalindrome(string)
+        timing = time.time() - start
+        print()
+        print(string[:10], "..." if len(string) > 10 else "")
+        print(result)
+        print(f"Done in {timing}")
 
 if __name__ == "__main__":
+    import time
     main()
