1b6eda3e981fe7f41cd7e1883d333d4bbd606ddd
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-17 17:59:49 +0800

    0005 Fast search in reverse string

--- 0005-longest-pali-substr.py
+++ 0005-longest-pali-substr.py
@@ -2,14 +2,59 @@
 '''
 
 
-class Solution(object):
+class Solution():
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
+
+        # Part 1: fast string search based on tokens
+
+        token_size = 4
+        # We'll keep them in the dictionary as {"token": position,}
+        reversed_tokens = {}
+
+        reversed_string = s[::-1]
+
+        # Generate dict of reversed tokens
+        for i in range(len(reversed_string) - token_size + 1):
+
+            # if token present several times, keep all it's positions
+            reversed_token = reversed_string[i:i+token_size]
+            if reversed_token not in reversed_tokens:
+                reversed_tokens[reversed_token] = [len(reversed_string)-i]
+            else:
+                reversed_tokens[reversed_token].append(len(reversed_string)-i)
+
+        # Go through the string again
+        for i in range(len(s) - token_size + 1):
+            # Generating straight tokens this time
+            straight_token = s[i:i+token_size]
+
+            # If straight token match a reverse token
+            if straight_token in reversed_tokens:
+                start = i
+
+                # Go through all instances where reverse token was found
+                for end in reversed_tokens[straight_token]:
+
+                    # Token should be "facing" each other, otherwise ignore them
+                    if end <= start:
+                        continue
+
+                    # And check if this string is a pali
+                    for shift in range((end - start) // 2):
+                        if s[start + shift] != s[end - shift - 1]:
+                            break
+                    else:
+                        # Return the first found one (it should be the longest)
+                        return s[start:end]
+
+        # Part 2: Look for palis smaller than the token size
+
         # Go through all possible lengths of pali from the biggest to 2
-        for length in range(len(s), 1, -1):
+        for length in range(token_size, 1, -1):
 
             # Go through all positions where pali of that length can occur
             for start in range(0, len(s) - length + 1):
@@ -36,13 +81,15 @@ def main():
     solution = Solution()
 
     test_cases = [
-        #"a",
-        #"bb",
-        #"babad", # "bab" or "aba"
-        #"cbbd", # bb
-        #"a" * 1000,
-        #"ab" * 500,
-        #"abcd" * 250 + "d",
+        "a",
+        "bb",
+        "babad", # "bab" or "aba"
+        "cbbd", # bb
+        "a" * 1000,
+        "ab" * 500,
+        "abcd" * 250 + "d",
+        "abcdefghijklmnopabbarstuvdcbawxyz",
+        "abcdefghijklmnopqdcbarstuvwxyz",
         "glwhcebdjbdroiurzfxxrbhzibilmcfasshhtyngwrsnbdpzgjphujzuawbebyhvxfhtoozcitaqibvvowyluvdbvoqikgojxcefzpdgahujuxpiclrrmalncdrotsgkpnfyujgvmhydrzdpiudkfchtklsaprptkzhwxsgafsvkahkbsighlyhjvbburdfjdfvjbaiivqxdqwivsjzztzkzygcsyxlvvwlckbsmvwjvrhvqfewjxgefeowfhrcturolvfgxilqdqvitbcebuooclugypurlsbdfquzsqngbscqwlrdpxeahricvtfqpnrfwbyjvahrtosovsbzhxtutyfjwjbpkfujeoueykmbcjtluuxvmffwgqjgrtsxtdimsescgahnudmsmyfijtfrcbkibbypenxnpiozzrnljazjgrftitldcueswqitrcvjzvlhionutppppzxoepvtzhkzjetpfqsuirdcyqfjsqhdewswldawhdyijhpqtrwgyfmmyhhkrafisicstqxokdmynnnqxaekzcgygsuzfiguujyxowqdfylesbzhnpznayzlinerzdqjrylyfzndgqokovabhzuskwozuxcsmyclvfwkbimhkdmjacesnvorrrvdwcgfewchbsyzrkktsjxgyybgwbvktvxyurufsrdufcunnfswqddukqrxyrueienhccpeuqbkbumlpxnudmwqdkzvsqsozkifpznwapxaxdclxjxuciyulsbxvwdoiolgxkhlrytiwrpvtjdwsssahupoyyjveedgqsthefdyxvjweaimadykubntfqcpbjyqbtnunuxzyytxfedrycsdhkfymaykeubowvkszzwmbbjezrphqildkmllskfawmcohdqalgccffxursvbyikjoglnillapcbcjuhaxukfhalcslemluvornmijbeawxzokgnlzugxkshrpojrwaasgfmjvkghpdyxt",
     ]
 
