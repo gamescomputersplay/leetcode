dc2b037e5a5ed88b25fa5d62f514c91784b5940f
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-18 17:57:12 +0800

    0007 no-cheating solution (better too)

--- 0007-reverse-int.py
+++ 0007-reverse-int.py
@@ -3,8 +3,34 @@
 
 class Solution:
 
-
     def reverse(self, x):
+        ''' Let's try to do it without the string reversal
+        '''
+        # Keep track if the number was negative
+        negative = True if x < 0 else False
+        x = abs(x)
+
+        rev_x = 0
+
+        # This cycle will copy the number over digit by digit
+        while x > 0:
+            
+            rev_x *= 10
+            rev_x += x % 10
+            x = x // 10
+
+        # Put the sign back on 
+        if negative:
+            rev_x *= -1
+        
+        # Kinda weird to do this check in Python, but okay
+        if  rev_x < - (2 ** 31) or rev_x > 2**31 - 1:
+            return 0
+
+        return rev_x
+
+
+    def reverse_cheating(self, x):
         ''' Works, but feels like cheating using string reversal
         '''
 
@@ -20,6 +46,16 @@ class Solution:
             return 0
 
         return rev_x
+
+
+def run_with_time(func):
+    ''' Time function func
+    '''
+
+    start = time.time()
+    for _ in range(1000000):
+        func(2**29)
+    print(f"Done in {time.time() - start}")
 
 
 def main():
@@ -38,6 +74,9 @@ def main():
     for number in test_cases:
         print(number, solution.reverse(number))
 
+    run_with_time(solution.reverse)
+    run_with_time(solution.reverse_cheating)
 
 if __name__ == "__main__":
+    import time
     main()
