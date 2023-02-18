cd127e0d78a7a225e118821b30699abb099b35a2
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-18 17:12:14 +0800

    0007 Solved, but feels like cheating

--- 0007-reverse-int.py
+++ 0007-reverse-int.py
@@ -2,8 +2,25 @@
 '''
 
 class Solution:
+
+
     def reverse(self, x):
-        pass
+        ''' Works, but feels like cheating using string reversal
+        '''
+
+        negative = True if x < 0 else False
+
+        x = abs(x)
+        rev_x = int(str(x)[::-1])
+
+        if negative:
+            rev_x *= -1
+        
+        if  rev_x < - (2 ** 31) or rev_x > 2**31 - 1:
+            return 0
+
+        return rev_x
+
 
 def main():
     ''' test reverse
@@ -11,7 +28,10 @@ def main():
     test_cases = [
         123,
         -123,
-        120
+        120,
+        987387676528325435243534523453453020746,
+        2**29,
+        2**31
     ]
 
     solution = Solution()
