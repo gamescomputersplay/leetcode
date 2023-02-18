7cdb864b47179435079ba88f03b39fbbadce5280
gamescomputersplay <kvasov.dmi@gmail.com>
2023-02-19 01:40:38 +0800

    0008 Solved

--- 0008-str-to-int-atoi.py
+++ 0008-str-to-int-atoi.py
@@ -4,7 +4,45 @@
 class Solution:
     def myAtoi(self, s):
 
-        return 0
+        # List of digits
+        numbers = "0123456789"
+        numbers_value = {numbers[i]: i for i in range(10)}
+
+        # The number we'll return
+        the_number = 0
+
+        # What position of the string we are at
+        pointer = 0
+
+        # Sign (1 or -1)
+        sign = 1
+
+        # Ignoring leading spaces
+        while pointer < len(s) and s[pointer] == " ":
+            pointer += 1
+
+        # Read the sign
+        if pointer < len(s) and s[pointer] == "-":
+            sign = -1
+        if pointer < len(s) and s[pointer] in ("-", "+"):
+            pointer += 1
+
+        # Read the number while there are digits
+        while pointer < len(s) and s[pointer] in numbers:
+
+            the_number *= 10
+            the_number += numbers_value[s[pointer]]
+            pointer += 1
+
+        the_number *= sign
+
+        # Kinda weird to do this check in Python, but okay
+        if  the_number < - (2 ** 31):
+            return - (2 ** 31)
+        if the_number > 2**31 - 1:
+            return 2**31 - 1
+
+        return  the_number
 
 def main():
     ''' Test myAtoi
@@ -15,6 +53,9 @@ def main():
         "42",
         "   -42",
         "4193 with words",
+        "0032",
+        "abc",
+        ""
     ]
     for string in test_cases:
         print(string, solution.myAtoi(string))
