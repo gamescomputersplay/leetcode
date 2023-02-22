931535de35e0b1b6a9f3a8772686539f4d8ab285
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-22 11:10:21 +0800

    1011 Slow (by 1 increment) solution

--- 1011-ship-within-d-days.py
+++ 1011-ship-within-d-days.py
@@ -2,9 +2,46 @@
 '''
 
 class Solution:
+    def is_shipping_possible(self, weights, capacity, days):
+        ''' Is it possible to do the shipping with this
+        capacity, in that many days
+        '''
+        pointer = 0
+
+        # Simulate loading by going through all the days
+        for _ in range(days):
+
+            # Load is emptied at the beginning of each day
+            daily_load = 0
+
+            # And then we keep putting stuff on as long as there is room
+            while daily_load + weights[pointer] <= capacity:
+                daily_load += weights[pointer]
+
+                # Moving along the conveyer as we do
+                pointer += 1
+
+                # If we used up all weights - task is doable
+                if pointer == len(weights):
+                    return True
+
+        return False
+
     def shipWithinDays(self, weights, days):
         
         return -1
+
+    def shipWithinDaysBrute(self, weights, days):
+        ''' Same, but by 1 increments
+        '''
+        capacity = max(max(weights), sum(weights)//days)
+
+        while True:
+            if self.is_shipping_possible(weights, capacity, days):
+                return capacity
+            capacity += 1
+
+
 
 def main():
     ''' Test shipWithinDays
@@ -17,7 +54,7 @@ def main():
         ([1,2,3,1,1], 4), #3
     ]
     for weights, days in test_cases:
-        result = solution.shipWithinDays(weights, days)
+        result = solution.shipWithinDaysBrute(weights, days)
         print(weights, days, result)
 
 if __name__ == "__main__":
