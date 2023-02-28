c4dc0e1ad2b4b7bd23aa3a02e2603cd274d920e8
gamescomputersplay <gamescomputerplay@gmail.com>
2023-02-28 12:49:36 +0800

    0010 Non-masks: recursively remaining parts

--- 0010-regexp-match.py
+++ 0010-regexp-match.py
@@ -7,7 +7,20 @@ class Solution:
         ''' Return whether string s is matching the regexp pattern p
         '''
 
-        def find_non_mask(pattern):
+        def find_occurrences_indexes(haystack, needle):
+            ''' Find all indexes at which "needle" occur in "haystack"
+            '''
+            search_from = 0
+            found_occurrences = []
+            while True:
+                found_one = haystack.find(needle, search_from)
+                if found_one == -1:
+                    return found_occurrences
+                found_occurrences.append(found_one)
+                search_from = found_one + 1
+
+
+        def find_first_non_mask(pattern):
             ''' Find the first occurrence of a non-mask (that is without "."
             or "*") in the pattern
             '''
@@ -58,8 +71,40 @@ class Solution:
                     end_non_mask += 1
 
             return pattern[start_non_mask:end_non_mask]
+        
+        # Find the first occurrence of a "non-mask" in the pattern,
+        # a fragment that has no "."" or "[character]*" in it
+        first_non_mask = find_first_non_mask(p)
+        print("Non-mask in", p, "is", first_non_mask)
+
+        if first_non_mask:
+            # If the string does not have it - failed match
+            if first_non_mask not in s:
+                return False
             
-        print("Non-mask", find_non_mask(p))
+            # Otherwise find all occurrence of non-mask
+            # in the searched string and pattern, permutate them
+            for p_non_mask in find_occurrences_indexes(p, first_non_mask):
+                for s_non_mask in find_occurrences_indexes(s, first_non_mask):
+
+                    # Break both s and p by the non-mask
+                    left_s, left_p = s[:s_non_mask], p[:p_non_mask]
+                    right_s, right_p = s[s_non_mask + len(first_non_mask):], \
+                        p[p_non_mask + len(first_non_mask):]
+
+                    # And recursively try matches in both halves
+                    # Match successful if moth halves match
+                    print("Recursion for", left_s, left_p, "and", right_s, right_p)
+                    if self.isMatch(left_s, left_p) and self.isMatch(right_s, right_p):
+                        return True
+            
+            # After trying all possible halves, no luck: match fail
+            return False
+
+        # Placeholder so recursion would work correctly
+        return True
+
+        
 
 def main():
     ''' test reverse
@@ -71,11 +116,11 @@ def main():
         ("aabbccdee", "aab*cc.ee"), # True
         ("aaaaaaaab", "a*b"), # True
         ("aaaaaaaab", "a*a*b"), # True
-        ("aaaaaaaab", "a*a*b"), # True
+        ("aaaaaaaab", "cca*a*b"), # True
         ("aaaaaaxxxb", "a*.*b*"), # True
         ("aaabbbxxxb", "a*.*.*b*"), # True
         ("aaaaaaxxxb", "a*.*.*b*"), # True
-        ("aaaaaaaaab", "a*.*.*b*"), # True
+        ("aaaaccaaaaaddaaaaa", "a*cc.*adda*"), # True
     ]
 
     solution = Solution()
