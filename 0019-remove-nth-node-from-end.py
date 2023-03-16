fde0393418bdb595598e460703abaf58547fab64
gamescomputersplay <gamescomputerplay@gmail.com>
2023-03-16 12:12:24 +0800

    0019 2-pass solution (better time and memory)

--- 0019-remove-nth-node-from-end.py
+++ 0019-remove-nth-node-from-end.py
@@ -4,7 +4,45 @@
 from classes import linkedlist
 
 class Solution:
+
     def removeNthFromEnd(self, head, n):
+
+        # One-link list. return None
+        # (n is assumed to be always valid)
+        if head.next is None:
+            return None
+
+        # Count total nodes
+        total = 0
+        current = head
+        while current != None:
+            total += 1
+            current = current.next
+
+        # Head is to be removed, return second link
+        if n == total:
+            return head.next
+
+        # Find link before the cut
+        before = head
+        for _ in range(total-n-1):
+            before = before.next
+
+        # Last link to be removed
+        if n == 1:
+            before.next = None
+            return head
+
+        # We need to cut and saw the list
+        # The one ofter the cut is 2 links forward
+        after = before.next
+        after = after.next
+
+        before.next = after
+
+        return head
+
+    def removeNthFromEndOnePass(self, head, n):
 
         # One-link list. return None
         # (n is assumed to be always valid)
@@ -31,6 +69,8 @@ class Solution:
         nodelist[-n-1].next = nodelist[-n+1]
         return head
 
+
+
 def main():
     ''' Test the twoSum
     '''
@@ -47,8 +87,27 @@ def main():
     solution = Solution()
     for list_1, n in test_cases:
         print("IN:", list_1, n)
+        #print("OUT:", solution.removeNthFromEndOnePass(list_1, n), "\n")
         print("OUT:", solution.removeNthFromEnd(list_1, n), "\n")
+
+def test_large_list(size):
+    ''' Test one large list with timing
+    '''
+    solution = Solution()
+    test_list = linkedlist.create_linked_list([i for i in range(size)])
+    the_node = size //2
+    start = time.time()
+    #solution.removeNthFromEndOnePass(test_list, the_node)
+    solution.removeNthFromEnd(test_list, the_node)
+    elapsed = time.time() - start
+    print(f"{size}: {elapsed}")
+
+def test_speed():
+    for power in range(20, 25):
+        test_large_list(2** power)
 
 
 if __name__ == "__main__":
-    main()
+    import time
+    #main()
+    test_speed()
