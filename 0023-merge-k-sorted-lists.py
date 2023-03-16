e67a29605cbeb43cc9b88a07e1128ff35b512fa6
gamescomputersplay <gamescomputerplay@gmail.com>
2023-03-16 18:46:46 +0800

    0023 WIP (set up linked list of linked lists)

--- 0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists.py
@@ -2,10 +2,53 @@
 '''
 
 from classes import linkedlist
+from classes.linkedlist import ListNode
 
 class Solution:
     def mergeKLists(self, lists):
-        return []
+
+        # Sanitation: remove lists with no links
+        for i in range(len(lists)):
+            if not lists[i]:
+                del lists[i]
+
+        # Return None if input list is empty
+        if not lists:
+            return None
+
+        # Sort the initial lists by value
+        lists.sort(key=lambda x: x.val)
+
+        # Convert list into a linked list
+        head = ListNode(lists[0])
+        current = head
+        for another_list in lists[1:]:
+            new_link = ListNode(another_list)
+            current.next = new_link
+            current = new_link
+
+        print(head)
+
+        result = None
+
+
+        while head is not None:
+            print(head)
+
+            # Pop the smallest element (first link of the first list)
+            smallest_link = head.val
+            print(smallest_link.val)
+
+            # Replace it with the next link
+            if smallest_link.next is not None:
+                head.val = smallest_link.next
+            # Or, if first linked list ends, use the second link
+            else:
+                head = head.next
+
+            # Move the head to an appropriate place down the list
+
+        return result
     
 
 def main():
@@ -15,17 +58,19 @@ def main():
     test_cases = [
         [
             linkedlist.create_linked_list([1,4,5]),
+            linkedlist.create_linked_list([2,6]),
             linkedlist.create_linked_list([1,3,4]),
-            linkedlist.create_linked_list([2,6])
         ],
-        [],
-        [[]],
+        #[],
+        #[[]],
     ]
 
     solution = Solution()
     for lists in test_cases:
-        print(f"IN: {[str(l) for l in lists]}\nOUT:{solution.mergeKLists(lists)}\n")
 
+        print(f"IN: {[str(l) for l in lists]}")
+        result = solution.mergeKLists(lists)
+        print(f"OUT:{result}\n")
 
 if __name__ == "__main__":
     main()
