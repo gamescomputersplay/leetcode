bae79c310b4e349fa70d2c054ee105da3edca11b
gamescomputersplay <gamescomputerplay@gmail.com>
2023-04-03 18:26:02 +0800

    0881 Solved (meh timing)

---
+++ 0881-boats-to-save-people.py
@@ -0,0 +1,88 @@
+''' https://leetcode.com/problems/boats-to-save-people/
+'''
+
+class Solution:
+    def numRescueBoats(self, people, limit):
+        people.sort()
+
+        # Triage people into:
+
+        # Chubbies: Maybe can share a boat with a skinny,
+        # but otherwise require a whole boat
+        chubbies = [person for person in people if person > limit / 2]
+
+        # Skinnies: Maybe can share a boat with a chubby,
+        # but otherwise fit 2 in a boat
+        skinnies = [person for person in people if person < limit / 2]
+
+        # Can share a boat with self or any skinny
+        midsizes = [person for person in people if person == limit / 2]
+
+        #print(skinnies, midsizes, chubbies)
+        
+        # Pointer to see how many skinnies can boat up with chubbies
+        skinny_pointer = len(skinnies) - 1
+
+        # Track the number of successful chubby-skinny matches
+        found_pairs = 0
+
+        # Flag that there are no more possible matches
+        matching_possible = True
+
+        # Go through the chubsters
+        for chubby in chubbies:
+
+            # Trying to find a suitable skinny for him
+            while True:
+
+                # Reached the end of the skinny list: no more matches
+                if skinny_pointer < 0:
+                    matching_possible = False
+                    break
+
+                # Found a skinny that maximized boat load
+                if chubby + skinnies[skinny_pointer] <= limit:
+                    #print("Match", chubby,  skinnies[skinny_pointer])
+                    found_pairs += 1
+                    skinny_pointer -= 1
+                    break
+
+                skinny_pointer -= 1
+
+            if not matching_possible:
+                break
+        
+        # Total number of boats is:
+        # - Successful chubby-skinny pairs (1 pair per boat)
+        # - Chubbies who didn't find a pair (1 chubster per boat)
+        # - Remaining skinnies and midsizes (2 per boat)
+        boats = found_pairs + \
+                (len(chubbies) - found_pairs) + \
+                (len(skinnies) - found_pairs + len(midsizes)) / 2
+
+        #print(found_pairs, (len(chubbies) - found_pairs), (len(skinnies) - found_pairs + len(midsizes)) / 2)
+
+        # Ceiling the number of boats
+        return int(boats + 0.5)
+
+def main():
+    ''' Test numRescueBoats
+    '''
+    solution = Solution()
+
+    test_cases = [
+        ([1,2], 3), # 1
+        ([3,2,2,1], 3), # 3
+        ([3,5,3,4], 5), # 4
+        ([1, 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9], 10),
+        ([1], 1),
+        ([1], 2),
+
+
+    ]
+    for people, limit in test_cases:
+        result = solution.numRescueBoats(people, limit)
+        print(f"{people[:10]}, {limit}, {result}\n")
+
+if __name__ == "__main__":
+    main()
