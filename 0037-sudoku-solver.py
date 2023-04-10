1ca7a7c7d2fcd40253b80815a651b5eef00077e8
gamescomputersplay <gamescomputerplay@gmail.com>
2023-04-10 16:08:05 +0800

    0037 Accepted, although a bit slow

--- 0037-sudoku-solver.py
+++ 0037-sudoku-solver.py
@@ -8,51 +8,47 @@ class Solution:
             ''' Check attempt matrix: does placing number in i j
             results in valid sudoku?
             '''
-            number = attempt[i][j]
+            number = board[i][j]
             for n in range(9):
-                if attempt[n][j] == number and n != i:
+                if board[n][j] == number and n != i:
                     return False
-                if attempt[i][n] == number and n != j:
+                if board[i][n] == number and n != j:
                     return False
-                x, y = i//3*3 + n%3, j//3*3 + n//3
-                #print(n, x, y)
-                if attempt[x][y] == number and x != i and y != j:
+                x, y = houses[(i,j)][n]
+                if board[x][y] == number and x != i and y != j:
                     return False
             return True
 
-        # Create a work copy with lists of options in empty cells
-
-        options = []
-        attempt = []
-
+        # Dict of cells and their options
+        options = {}
         for i in range(9):
-            options.append([])
-            attempt.append([])
             for j in range(9):
-                if board[i][j] != ".":
-                    options[-1].append(board[i][j])
-                    attempt[-1].append(board[i][j])
-                else:
-                    options[-1].append([str(i) for i in range(1, 10)])
-                    attempt[-1].append(".")
-
-        #print(options)
+                if board[i][j] == ".":
+                    options[(i, j)] = [str(i) for i in range(1, 10)]
 
         # Backtracking stack. It will contain backtracking point as:
-        # [(i, j, index of element to try), ...]
+        # [(i, j, index of next element to try), ...]
         stack = []
 
+        # Helper to optimize checking houses
+        houses = {}
+        for i in range(9):
+            for j in range(9):
+                houses[(i,j)] = []
+                for n in range(9):
+                    houses[(i,j)].append((i//3*3 + n%3, j//3*3 + n//3))
+
+
+        # Go through all the cells
         i, j = 0, 0
-
-        for _ in range(100000):
-
-            
+        while True:
 
             # This is to move to the next line when appropriate 
             if j == 9:
                 i += 1
                 j =0
-            # If we passed through all rows - break from the while
+
+            # If we passed through all rows - break from the while, we are done
             if i == 9:
                 break
 
@@ -62,14 +58,14 @@ class Solution:
                 # Pop the stack value
                 stack_i, stack_j, stack_attempt = stack.pop()
                 # If the next attempt does not exist: remove this backtracking point
-                if stack_attempt > len(options[i][j]) - 1:
-                    attempt[i][j] = "."
+                if stack_attempt > len(options[(i, j)]) - 1:
+                    board[i][j] = "."
                     i, j, _ = stack[-1]
                     # print(f"Exhausted all option, going back to {i}, {j}")
                     continue
 
                 # If the attempt valid - try next one
-                attempt[i][j] = options[i][j][stack_attempt]
+                board[i][j] = options[(i, j)][stack_attempt]
                 # print(f"Trying value {options[i][j][stack_attempt]}, at {i} {j}")
                 stack.append((i, j, stack_attempt + 1))
 
@@ -78,7 +74,7 @@ class Solution:
                     j += 1
 
             # If the options are fixed number - ignore it and move on
-            elif not isinstance(options[i][j], list):
+            elif (i, j) not in options:
                 # print(i, j, options[i][j])
                 j += 1
 
@@ -86,8 +82,6 @@ class Solution:
             else:
                 # print(f"Set backtracking point at {i}, {j}")
                 stack.append((i, j, 0))
-
-        print(attempt)
 
         return
 
@@ -98,17 +92,22 @@ def main(verbose=True):
 
     test_cases = [
         [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]],
-        [[".",".","4","6","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+        # [[".",".","4","6","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
     ]
     for sudoku in test_cases:
+        if verbose:
+            print("IN")
+            for line in sudoku:
+                print(" ".join(line))
         solution.solveSudoku(sudoku)
         if verbose:
+            print("OUT")
             for line in sudoku:
                 print(" ".join(line))
 
 def timing():
     start = time.time()
-    for _ in range(10000):
+    for _ in range(30):
         main(verbose=False)
     elapsed = time.time() - start
     print(elapsed)
@@ -116,4 +115,4 @@ def timing():
 if __name__ == "__main__":
     import time
     main()
-    #timing()
+    timing()
