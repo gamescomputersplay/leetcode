27f86554caa958547ed334d07d561e8ab5dabd39
gamescomputersplay <gamescomputerplay@gmail.com>
2023-04-14 12:10:25 +0800

    0053 Tried to optimize, but still quite slow

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -6,29 +6,32 @@ class Solution:
 
         total = sum(nums)
 
-        # Minimum sum on the left side of the array
+        # Minimum sum on the right side of the array
         # [n] means n-th is the first element NOT in the sum 
-        min_sum_left = [0]
+        min_sum_right = [0]
         running_sum = 0
         # We skip the last element, because at least
         # one element has to be left in the subarray
-        for num in nums[:-1]:
-            running_sum += num
-            min_sum_left.append(min(min_sum_left[-1], running_sum))
-
-        # Same from the right
-        min_sum_right = [0]
-        running_sum = 0
         for num in nums[:0:-1]:
             running_sum += num
             min_sum_right.append(min(min_sum_right[-1], running_sum))
 
-        # Check which combination of total minus value in both arrays
-        # (which represents a central portion with minimal cuts
-        # from both sides) can produce the best sum
+        # Several things are done here
+        # - keep the track of the sum of the i left elements
+        # - keep track of teh minimum of such sum
+        # - keep track of 
+        # (best total - minimal sum from the left - minimal sum from the right)
         best_sum = float("-inf")
-        for i in range(len(min_sum_left)):
-            best_sum = max(best_sum, total - min_sum_left[i] - min_sum_right[-i-1])
+        running_sum = 0
+        min_sum_left = 0
+        for i, num in enumerate(nums):
+
+            min_sum_left = min(min_sum_left, running_sum)
+            best_sum = max(best_sum, total - min_sum_left - min_sum_right[-i-1])
+
+            # Sum update is in the end of the cycle, as as it is
+            # the sum of elements to the left of current item
+            running_sum += num
 
         return best_sum
     
@@ -42,7 +45,7 @@ class Solution:
 def random_test(runs=10):
     solution = Solution()
     for _ in range(runs):
-        nums = [random.randint(-10, 10) for _ in range(1, 10)]
+        nums = [random.randint(-10, 10) for _ in range(1, 5)]
         result = solution.maxSubArray(nums)
         result2 = solution.maxSubArray_brute(nums)
         if result2 != result:
@@ -63,6 +66,7 @@ def main():
         [-3, -2, -1, 10, -1, -2, -3],
         [0, -8, -10, -6, 2, -5, -3, -5, 8],
         [-6, -2, -3, 0],
+        [-3, -4, -7, -7]
     ]
     for nums in test_cases:
         result = solution.maxSubArray(nums)
