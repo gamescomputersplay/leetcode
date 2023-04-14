0df939dddbd9ab52d493ed4152d0ece876123edf
gamescomputersplay <gamescomputerplay@gmail.com>
2023-04-14 10:51:21 +0800

    0053 Solved, although slow

--- 0053-maximum-subarray.py
+++ 0053-maximum-subarray.py
@@ -5,7 +5,6 @@ class Solution:
     def maxSubArray(self, nums):
 
         total = sum(nums)
-        #print(total)
 
         # Minimum sum on the left side of the array
         # [n] means n-th is the first element NOT in the sum 
@@ -17,8 +16,6 @@ class Solution:
             running_sum += num
             min_sum_left.append(min(min_sum_left[-1], running_sum))
 
-        #print(min_sum_left)
-
         # Same from the right
         min_sum_right = [0]
         running_sum = 0
@@ -26,9 +23,9 @@ class Solution:
             running_sum += num
             min_sum_right.append(min(min_sum_right[-1], running_sum))
 
-        #print(min_sum_right[::-1])
-
-        # Check which combination can produce the best sum
+        # Check which combination of total minus value in both arrays
+        # (which represents a central portion with minimal cuts
+        # from both sides) can produce the best sum
         best_sum = float("-inf")
         for i in range(len(min_sum_left)):
             best_sum = max(best_sum, total - min_sum_left[i] - min_sum_right[-i-1])
@@ -37,10 +34,22 @@ class Solution:
     
     def maxSubArray_brute(self, nums):
         max_sum = sum(nums)
-        for i in range(len(nums)-1):
+        for i in range(len(nums)):
             for j in range(i+1, len(nums) + 1):
                 max_sum = max(max_sum, sum(nums[i:j]))
         return max_sum
+
+def random_test(runs=10):
+    solution = Solution()
+    for _ in range(runs):
+        nums = [random.randint(-10, 10) for _ in range(1, 10)]
+        result = solution.maxSubArray(nums)
+        result2 = solution.maxSubArray_brute(nums)
+        if result2 != result:
+            print(f"Error in {nums}")
+            break
+    else:
+        print(f"{runs} tests okay")
 
 def main():
     ''' Test maxSubArray
@@ -52,6 +61,8 @@ def main():
         [1],
         [5,4,-1,7,8],
         [-3, -2, -1, 10, -1, -2, -3],
+        [0, -8, -10, -6, 2, -5, -3, -5, 8],
+        [-6, -2, -3, 0],
     ]
     for nums in test_cases:
         result = solution.maxSubArray(nums)
@@ -59,4 +70,6 @@ def main():
         print(nums, result2, result, result2 == result)
 
 if __name__ == "__main__":
+    import random
     main()
+    random_test(10000)
