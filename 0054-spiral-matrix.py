7463ceac498a79354c4b1d8a1aa6c08891aac663
gamescomputersplay <kvasov.dmi@gmail.com>
2023-04-15 23:02:04 +0800

    0054 Solved

--- 0054-spiral-matrix.py
+++ 0054-spiral-matrix.py
@@ -4,30 +4,45 @@
 class Solution:
     def spiralOrder(self, matrix):
 
+        # Output
         output = []
-        size_x, size_y =  len(matrix), len(matrix[0]),
+        # Track visited to know when to turn
+        visited = set()
+        size_x, size_y =  len(matrix), len(matrix[0])
 
-        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        direction = 0
 
         x, y = 0, 0
 
-        for line_n in range(min(size_x, size_y) * 2):
+        # Keep going until all numbers  were read
+        while True:
 
-            line_length = (size_y, size_x)[line_n%2] - 1 - line_n // 4 * 2
-            dx, dy = directions[line_n%4]
-            print(line_length, dx, dy)
+            # Append the result, and track which coordinate were used
+            output.append(matrix[x][y])
+            if len(output) == size_x * size_y:
+                break
+            visited.add((x, y))
 
-            for i in range(line_length):
-                #print(x, y)
-                output.append(matrix[x][y])
-                x += dx
-                y += dy
- 
+            # Find the next direction to go / coordinate based on
+            # - it is not an edge
+            # - it has not been visited
+            while True:
+
+                dx, dy = directions[direction]
+                next_x, next_y = x + dx, y + dy
+
+                if 0 <= next_x < size_x and 0 <= next_y < size_y and (next_x, next_y) not in visited:
+                     x, y =  next_x, next_y
+                     break
+
+                direction += 1
+                direction %= 4
+
         return output
 
-def make_square(x, y):
-    ''' Make a square matrix size "size"
+def make_rect(x, y):
+    ''' Make a rectangle matrix size "size"
     '''
     result = []
     count = 1
@@ -51,8 +66,13 @@ def main():
 
     test_cases = [
         [[1,2,3],[4,5,6],[7,8,9]],
+        [[1,1,1],[1,1,1],[1,1,1]],
         [[1,2,3,4],[5,6,7,8],[9,10,11,12]],
-        make_square(6, 5)
+        make_rect(6, 5),
+        make_rect(1, 1),
+        make_rect(1, 10),
+        make_rect(10, 1),
+        
     ]
     for matrix in test_cases:
         print("IN")
@@ -61,5 +81,4 @@ def main():
         print(result)
 
 if __name__ == "__main__":
-    import random
     main()
