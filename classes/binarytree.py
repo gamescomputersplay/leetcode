a82aa1b738deca400bc38a9c065a46488dc478fc
gamescomputersplay <gamescomputerplay@gmail.com>
2023-04-19 14:34:41 +0800

    Add list representation

--- classes/binarytree.py
+++ classes/binarytree.py
@@ -11,9 +11,36 @@ class TreeNode:
         self.right = right
     
     def __str__(self):
-        ''' Text representation TODO
+        ''' Text representation of a tree node
         '''
-        return "Tree Node, Text representation TODO"
+
+        def add_item(array, item, position):
+            ''' Add item to the array at the position "position",
+            pad non-exiting position before it with None
+            '''
+            while len(array) < position:
+                array.append(None)
+            array.append(item)
+
+        list_representation = []
+
+        queue_pointer = 0
+        queue = [(self, 0)]
+
+        while len(queue) > queue_pointer:
+
+            item, item_position = queue[queue_pointer]
+            add_item(list_representation, item.val, item_position)
+
+            if item.left is not None:
+                queue.append((item.left, item_position * 2 + 1))
+            if item.right is not None:
+                queue.append((item.right, item_position * 2 + 2))
+
+            queue_pointer += 1
+
+        return "Tree Node: " + str(list_representation)
+
 
 def create_binary_tree(array):
     ''' Create a binary tree from a list.
@@ -37,5 +64,5 @@ def create_binary_tree(array):
 if __name__ == "__main__":
     print("This library is a definition and some helpers to work with binary trees")
     print("Here's a short demo:")
-    demo_list = [1, 2, 3, None, 4, 5, None]
+    demo_list = [1, 2, 3, None, 4, 5, None, 6, 7, None, 8]
     print(f"This is a tree created from {demo_list}: {create_binary_tree(demo_list)}")
