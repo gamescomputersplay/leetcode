4bdf7540316715837e883a82e47cfee8a118b6da
gamescomputersplay <gamescomputersplay@gmail.com>
2023-05-11 16:38:26 +0800

    0066 Solved but slow

---
+++ 0066-plus-one.py
@@ -0,0 +1,43 @@
+''' https://leetcode.com/problems/plus-one/
+'''
+
+# It breaks the input list, but looks like it's okay.
+
+class Solution:
+    def plusOne(self, digits):
+
+        carry = 1
+
+        for pos in range(len(digits)- 1, -1, -1):
+
+            n = digits[pos]
+            n += carry
+            if n == 10:
+                n = 0
+                carry = 1
+            else: carry = 0
+            digits[pos] = n
+
+        if carry == 1:
+            digits = [1] + digits
+
+        return digits
+
+def main():
+    ''' Test plusOne
+    '''
+    solution = Solution()
+
+    test_cases = [
+       [1, 2, 3],
+       [4, 3, 2, 1],
+       [9],
+       [9, 9, 9, 9]
+    ]
+
+    for digits in test_cases:
+        result = solution.plusOne(digits)
+        print(f"{digits} {result}\n")
+
+if __name__ == "__main__":
+    main()
