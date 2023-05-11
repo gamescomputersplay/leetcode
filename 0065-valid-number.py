5a4e305712ff5e8f93c5fa9c170159be074ae4e3
gamescomputersplay <kvasov.dmi@gmail.com>
2023-05-12 00:34:12 +0800

    0065 Another solution, but still slow

--- 0065-valid-number.py
+++ 0065-valid-number.py
@@ -10,39 +10,49 @@ class Solution:
         length = len(s)
         numbers = "1234567890"
 
-        for element in "-N.Ne-M":
-            # N for a number that can be empty
-            # M for a number that cannot be empty
-
+        # Sign
+        if pointer < length and s[pointer] in ("-", "+"):
+            pointer += 1
             if pointer == length:
-                break
+                return False
 
-            if element == "N":
-                while pointer < length and s[pointer] in numbers:
-                    pointer += 1
-                    at_least_one_N = True
-            
-            elif element == "-":
-                if s[pointer] in ("-", "+"):
-                    pointer += 1
+        # Number optional (1)
+        while pointer < length and s[pointer] in numbers:
+            pointer += 1
+            at_least_one_N = True
+        if pointer == length:
+                return at_least_one_N
+        # Dot
+        if s[pointer] == ".":
+            pointer += 1
+            if pointer == length:
+                return at_least_one_N
 
-            elif element == "e":
-                if s[pointer] in ("e", "E"):
-                    pointer += 1
-                    valid_E = False
-                else:
-                    break
-            
-            elif element == ".":
-                if s[pointer] == ".":
-                    pointer += 1
+        # Number optional (2)
+        while pointer < length and s[pointer] in numbers:
+            pointer += 1
+            at_least_one_N = True
+            if pointer == length:
+                return at_least_one_N
+        # Exponent
+        if s[pointer] in ("e", "E"):
+            pointer += 1
+            valid_E = False
+            if pointer == length:
+                return False
 
-            elif element == "M":
-                if pointer < length and s[pointer] not in numbers:
+            # Sign 2
+            if s[pointer] in ("-", "+"):
+                pointer += 1
+                if pointer == length:
                     return False
-                while pointer < length and s[pointer] in numbers:
-                    pointer += 1
-                    valid_E = True
+
+            # Number mandatory
+            if s[pointer] not in numbers:
+                return False
+            while pointer < length and s[pointer] in numbers:
+                pointer += 1
+                valid_E = True
 
         if not at_least_one_N or pointer != len(s) or not valid_E:
             return False
@@ -95,7 +105,7 @@ def main(verbose=True):
 
     test_cases = [ 
         "-1.5e-3",
-       "", "0", "2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789", # Valid
+       "0", "2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789", # Valid
        "e", ".", "abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53", "11.2.3", "11e22ee33e3", ".+", "+." # Non valid
     ]
 
@@ -105,6 +115,7 @@ def main(verbose=True):
             print(s, result)
 
 def test_timing(runs=10000):
+
     start = time.time()
     for _ in range(runs):
         main(verbose=False)
