b79d95047c73346e04b1423434738f8a42bb1747
gamescomputersplay <gamescomputersplay@gmail.com>
2023-07-06 14:52:46 +0800

    0089 Solved

---
+++ 0089-gray-code.py
@@ -0,0 +1,35 @@
+''' https://leetcode.com/problems/gray-code/
+'''
+
+class Solution:
+    def grayCode(self, n):
+
+        # Recursion end
+        if n==1:
+            return [0, 1]
+
+        # Forward
+        half = self.grayCode(n - 1)
+        # And backward with 1 in from
+        second_half = [2 ** (n-1) + x for x in half[::-1]]
+
+        return half + second_half
+
+def main():
+    ''' Test grayCode
+    '''
+    solution = Solution()
+
+    test_cases = [
+       1,
+       2,
+       3,
+       4
+
+    ]
+    for n in test_cases:
+        result = solution.grayCode(n)
+        print(f"{n}: {result}")
+
+if __name__ == "__main__":
+    main()
