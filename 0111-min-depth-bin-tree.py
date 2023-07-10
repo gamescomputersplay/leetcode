e5ba13f490d1bb00754751085d21057230029359
gamescomputersplay <gamescomputersplay@gmail.com>
2023-07-10 09:56:33 +0800

    0111 Solved

---
+++ 0111-min-depth-bin-tree.py
@@ -0,0 +1,37 @@
+''' https://leetcode.com/problems/minimum-depth-of-binary-tree/
+'''
+
+from classes import binarytree
+from classes.binarytree import TreeNode
+
+class Solution:
+    def minDepth(self, root):
+
+        if root is None:
+            return 0
+
+        if root.left is None and root.right is None:
+            return 1
+
+        left = float("inf") if root.left is None else self.minDepth(root.left)
+        right = float("inf") if root.right is None else self.minDepth(root.right)
+
+        return min(left, right) + 1
+
+def main():
+    ''' Test recoverTree
+    '''
+    solution = Solution()
+
+    test_cases = [
+        [3,9,20,None,None,15,7],
+        [2,None,3,None,4,None,5,None,6],
+        []
+    ]
+    for list_tree in test_cases:
+        tree = binarytree.level_order_2_tree(list_tree)
+        result = solution.minDepth(tree)
+        print(f"{tree}, {result}")
+
+if __name__ == "__main__":
+    main()
