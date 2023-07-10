8ea4647aeadd99a7c55c9a1a0cc9b6d4f374b697
gamescomputersplay <gamescomputersplay@gmail.com>
2023-07-10 10:01:24 +0800

    0111 Better timing

--- 0111-min-depth-bin-tree.py
+++ 0111-min-depth-bin-tree.py
@@ -13,10 +13,12 @@ class Solution:
         if root.left is None and root.right is None:
             return 1
 
-        left = float("inf") if root.left is None else self.minDepth(root.left)
-        right = float("inf") if root.right is None else self.minDepth(root.right)
+        if root.left is None:
+            return self.minDepth(root.right) + 1
+        if root.right is None:
+            return self.minDepth(root.left) + 1
 
-        return min(left, right) + 1
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
 
 def main():
     ''' Test recoverTree
@@ -26,7 +28,8 @@ def main():
     test_cases = [
         [3,9,20,None,None,15,7],
         [2,None,3,None,4,None,5,None,6],
-        []
+        [],
+        [1,2],
     ]
     for list_tree in test_cases:
         tree = binarytree.level_order_2_tree(list_tree)
