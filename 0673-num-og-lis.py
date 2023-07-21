21053525c1e2808e4d4a6c6df6611a27745eb444
gamescomputersplay <gamescomputersplay@gmail.com>
2023-07-21 12:38:57 +0800

    0673 Solved

--- 0673-num-og-lis.py
+++ 0673-num-og-lis.py
@@ -3,7 +3,37 @@
 
 class Solution:
     def findNumberOfLIS(self, nums):
-        return 0
+
+        longsest_lis = 0
+        count_of_longest_lis = 0
+
+        # [(num, len so far, cont so far)]
+        edges = []
+
+        for num in nums:
+
+            best_len_so_far = 0
+            best_count_so_far = 1
+
+            for edge, length, count in edges:
+                if edge < num:
+                    if length > best_len_so_far:
+                        best_len_so_far = length
+                        best_count_so_far = count
+                    elif length == best_len_so_far:
+                        best_count_so_far += count
+
+            edges.append((num, best_len_so_far + 1, best_count_so_far))
+            longsest_lis = max(longsest_lis, best_len_so_far + 1)
+
+            #print(num, edges)
+
+        # Iterate thorugh the list again and find the sum of counts of the longest chain
+        for _, length, count in edges:
+            if length == longsest_lis:
+                count_of_longest_lis += count
+
+        return count_of_longest_lis
 
 def main():
     ''' Test findNumberOfLIS
@@ -12,7 +42,10 @@ def main():
 
     test_cases = [
         [1,3,5,4,7], #2
-        [2,2,2,2,2], #2
+        [2,2,2,2,2], #5
+        [1],
+        [1, 2, 3], 
+        [3, 2, 1],
     ]
     for nums in test_cases:
         result = solution.findNumberOfLIS(nums)
