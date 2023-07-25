028ef56faf79baaa3ab4086f72e7738e4fd35be5
gamescomputersplay <gamescomputersplay@gmail.com>
2023-07-25 10:15:31 +0800

    0122 Faster

--- 0122-best-time-to-buy-and-sell-2.py
+++ 0122-best-time-to-buy-and-sell-2.py
@@ -4,14 +4,6 @@
 class Solution:
     def maxProfit(self, prices):
 
-        # Dedup conseq prices (remove same conseq numbers)
-        deduped = 0
-        for i, price in enumerate(prices[1:], start=1):
-            if price != prices[deduped]:
-                deduped += 1
-                prices[deduped] = price
-        prices = prices[:deduped+1]
-
         # Can't make money here
         if len(prices) == 1:
             return 0
@@ -26,13 +18,19 @@ class Solution:
         # Go through all items except the first and the last one
         for i in range(1, len(prices) - 1):
 
+            a, b, c = prices[i-1], prices[i], prices[i+1]
+
             # If it is a peak: sell
-            if prices[i-1] < prices[i] > prices[i+1]:
+            if bought_at is not None \
+               and (a < b or b > c) \
+               and a <= b >= c:
                 profit += prices[i] - bought_at
                 bought_at = None
 
             # if it is a trough: buy
-            if prices[i-1] > prices[i] < prices[i+1]:
+            if bought_at is None \
+               and (a > b or b < c) \
+               and a >= b <= c:
                 bought_at = prices[i]
 
         # Last one
@@ -52,6 +50,8 @@ def main():
         [1,2,3,4,5], #4
         [7,6,4,3,1], # 0
         [2, 2, 2, 2],
+        [2,2,2,2,1,1,1,3,3,3],
+        [2,1,2,0,1], #2
     ]
     for prices in test_cases:
         result = solution.maxProfit(prices)
