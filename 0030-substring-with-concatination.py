9f26e82355a19150798faf2e18821b72611f00bd
gamescomputersplay <gamescomputersplay@gmail.com>
2023-08-04 15:11:46 +0800

    0030 Slower, but I have an idea

--- 0030-substring-with-concatination.py
+++ 0030-substring-with-concatination.py
@@ -6,6 +6,43 @@ class Solution:
 
         result = []
 
+        word_len = len(words[0])
+        total_wordlen = word_len * len(words)
+
+        words_counts = {}
+        for word in words:
+            words_counts[word] = words_counts.get(word, 0) + 1
+
+        # Go through all starting positions
+        for start in range(len(s) - total_wordlen + 1):
+
+            # Break string into words from this position
+            found = {}
+            for chunk_n in range(len(words)):
+
+                chunk = s[start + chunk_n * word_len:start + (chunk_n + 1) * word_len]
+
+                # Now compare the chunks with the words (and counts) we have
+                if chunk in words_counts:
+                    found[chunk] = found.get(chunk, 0) + 1
+                    # It needs more chunks that we have - does not fit
+                    if found[chunk] > words_counts[chunk]:
+                        break
+                # If needs the chunk we dont have - does not fit
+                else:
+                    break
+
+            # No mismatches - this start position fits
+            else:
+                result.append(start)
+
+        return result
+
+
+    def findSubstring_old(self, s, words):
+
+        result = []
+
         # This is the word size we are looking for
         word_size = len(words[0])
 
@@ -39,11 +76,11 @@ class Solution:
                 if next_match not in substring_found_at:
                     break
             else:
-                
+
                 # At this point we know there is a place that matches
                 # the right number fo words in a row, but we don't know
                 # if those are the right words
-                
+
                 # We will break down the string, construct a check word
                 # and see if it matches original check word
                 new_words = [s[first_match_at + shift * word_size:first_match_at + (shift + 1) * word_size]
