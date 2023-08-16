bba7b0b2167ceb2afef6e3a0e86a7282b6cb38e8
gamescomputersplay <gamescomputersplay@gmail.com>
2023-08-16 16:07:42 +0800

    0178 Another method

--- 0178-rank-scores.py
+++ 0178-rank-scores.py
@@ -15,11 +15,15 @@ def order_scores(scores):
 
     rank = 1
     scores["rank"] = None
-    scores.sort_values("score", ascending=False, inplace=True)
+    
     scores = scores.groupby("score", sort=False).apply(addrank)
     scores.reset_index(drop=True, inplace=True)
     return scores[["score", "rank"]]
 
+def order_scores_built_in(scores):
+    scores.sort_values("score", ascending=False, inplace=True)
+    scores["rank"] = scores["score"].rank(method="dense", ascending=False)
+    return scores[["score", "rank"]]
 
 if __name__ == "__main__":
     data = [[1, 3.5], [2, 3.65], [3, 4.0], [4, 3.85], [5, 4.0], [6, 3.65]]
