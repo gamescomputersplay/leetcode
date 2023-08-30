5fc840f9bbd208a072b440c4d8d128d5eebb7ad9
gamescomputersplay <gamescomputersplay@gmail.com>
2023-08-30 12:04:11 +0800

    0119 Better memory

--- 0119-pascals-triangle-2.py
+++ 0119-pascals-triangle-2.py
@@ -4,7 +4,14 @@
 class Solution:
     def getRow(self, rowIndex):
 
-        result = [1 for _ in range(rowIndex + 1)]
+        result = [1] + [0 for _ in range(rowIndex)]
+
+        # Perform rowIndex steps to go from [1] the result line
+        for i in range(rowIndex):
+
+            # Sum current list with current list moved to the right 1 positions
+            for j in range(i+1, 0, -1):
+                result[j] += result[j - 1]
 
         return result
 
@@ -14,7 +21,7 @@ def main():
     solution = Solution()
 
     test_cases = [
-        num for num in range(4)
+        num for num in range(14)
     ]
     for numRows in test_cases:
         result = solution.getRow(numRows)
