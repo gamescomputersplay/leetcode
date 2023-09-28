3c7e0d809cce395f6dcb6bdc7e698c44d772de9f
gamescomputersplay <gamescomputersplay@gmail.com>
2023-09-28 11:49:03 +0800

    0151 More pythonic way

--- 0151-reverse-words-in-string.py
+++ 0151-reverse-words-in-string.py
@@ -3,18 +3,8 @@
 
 class Solution:
     def reverseWords(self, s):
-        words = []
-        curr_word_start = None
-
-        # Break the string into words by spaces
-        for pos, ch in enumerate(s + " "):
-            if ch != " " and curr_word_start is None:
-                curr_word_start = pos
-            if ch == " " and curr_word_start is not None:
-                words.append(s[curr_word_start: pos])
-                curr_word_start = None
-
-        return " ".join(words[::-1])
+        words = s.split(" ")
+        return " ".join([word for word in words if word][::-1])
 
 def main():
     ''' Test reverseWords
