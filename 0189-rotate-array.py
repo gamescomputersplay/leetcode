393d24d11b3b4105b21e0eb82b671f96827f7ce0
gamescomputersplay <gamescomputersplay@gmail.com>
2024-07-17 15:30:21 +0800

    0189 Solved

--- 0189-rotate-array.py
+++ 0189-rotate-array.py
@@ -2,9 +2,15 @@
 '''
 
 class Solution:
-    def rotate(self, nums, k):
+    def rotate_slow(self, nums, k):
         for _ in range(k):
             nums.insert(0, nums.pop())
+
+    def rotate(self, nums, k):
+        size = (len(nums)-k) % len(nums) 
+        nums.extend(nums[:size])
+        del nums[:size]
+
 
 def main():
     ''' Test rotate
@@ -14,11 +20,12 @@ def main():
     test_cases = [
         ([1,2,3,4,5,6,7], 3),
         ([-1,-100,3,99], 2),
+        ([1,2], 3)
     ]
     for nums, k in test_cases:
         print(nums, k)
         solution.rotate(nums, k)
-        print(nums)
+        print(nums, "\n")
 
 if __name__ == "__main__":
     main()
